a15fb7ad72f09142674f368e524a201ccaa204d2
sazed1237 <sazedulislam9126@gmail.com>
2024-09-03 00:41:54 +0600

    cors problem solve

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
+    exposedHeaders: ['Authorization'],
 }))
 
+app.options('*', cors());
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api', router)
+
 
 
 // connection
