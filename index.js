4662b295cad2ad67de138b2aaa6b66f1e6f891f5
sazed1237 <sazedulislam9126@gmail.com>
2024-09-03 00:48:19 +0600

    frontend production url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || process.env.FRONTEND_PRODUCTION_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
